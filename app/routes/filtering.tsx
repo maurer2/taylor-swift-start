74e9637b3e1164b668469d13ecd03d459cedb343
Martin <mr.mmaurer+work@gmail.com>
2025-03-05 21:15:53 +0100

    Added non-suspense loading indicator

--- app/routes/filtering.tsx
+++ app/routes/filtering.tsx
@@ -1,17 +1,25 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { getListEntries } from "~/server-functions/get-list-entries";
-import { useServerFn } from "@tanstack/react-start";
-import { use } from "react";
+// import { useServerFn } from "@tanstack/react-start";
+// import { use } from "react";
 
 export const Route = createFileRoute("/filtering")({
   component: Filtering,
+  async loader() {
+    const list = await getListEntries();
+
+    return {
+      list,
+    };
+  },
+  staleTime: 0,
 });
 
 function Filtering() {
-  const getListEntriesFn = useServerFn(getListEntries);
-  const list = use(getListEntriesFn()); // infinite loop
-
-  console.log("list", list);
+  // const getListEntriesFn = useServerFn(getListEntries);
+  // const list = use(getListEntriesFn()); // infinite loop
+  const { list } = Route.useLoaderData();
+  const { isFetching } = Route.useMatch();
 
   return (
     <div className="p-2">
@@ -21,7 +29,12 @@ function Filtering() {
 
       <hr />
 
-      <pre>{JSON.stringify(list, null, 4)}</pre>
+      {isFetching ? <p>Loading</p> : null}
+      {!isFetching ? (
+        <div aria-live="polite" aria-busy={isFetching}>
+          <pre>{JSON.stringify(list, null, 4)}</pre>
+        </div>
+      ) : null}
     </div>
   );
 }
--- app/server-functions/get-list-entries/index.ts
+++ app/server-functions/get-list-entries/index.ts
@@ -7,25 +7,15 @@ type ListEntry = {
   country: string;
 };
 
-const MIN_DURATION = 1;
+const MIN_DURATION = 2000;
 
 export const getListEntries = createServerFn().handler(async () => {
-  const { resolve, promise: promiseList } =
-    Promise.withResolvers<ListEntry[]>();
+  const promises = await Promise.all([
+    new Promise<ListEntry[]>((resolve) => resolve(listJSON as ListEntry[])),
+    new Promise<void>((resolve) => setTimeout(() => resolve(), MIN_DURATION)),
+  ]).catch((error: unknown) => {
+    throw new Error("Error", { cause: error });
+  });
 
-  // const promises = await Promise.all([
-  //   promiseList,
-  //   new Promise((resolveMinDuration) =>
-  //   // setTimeout(() => resolveMinDuration(null), MIN_DURATION)
-  //   ),
-  // ]).catch((error: unknown) => {
-  //   throw new Error("Error", { cause: error });
-  // });
-
-  setTimeout(() => {
-    resolve(listJSON as ListEntry[]);
-  }, 1000);
-
-  return promiseList;
-  // return promises[0];
+  return promises[0];
 });
