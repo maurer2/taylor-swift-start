5003f886486fe5e2b2408638848a215ecbe46992
Martin <mr.mmaurer@gmail.com>
2025-09-27 22:25:19 +0200

    Fix gitignore

--- dist/client/assets/index-PwxPRzjc.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-DQ8XdR0N.js";function m(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})}export{m as component};
--- dist/server/assets/_tanstack-start-server-fn-manifest_v-CHvndpiZ.js
+++
@@ -1,7 +0,0 @@
-const _tanstackStartServerFnManifest_v = { "src_server-functions_get-list-entries_index_ts--getListEntries_createServerFn_handler": {
-  functionName: "getListEntries_createServerFn_handler",
-  importer: () => import("./index-3v2024KY.js")
-} };
-export {
-  _tanstackStartServerFnManifest_v as default
-};
